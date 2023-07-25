de276b759a2c9719c32b96b5b7db7216552a9ede
roland_quaye <rquaye15@hotmail.co.uk>
2023-07-25 19:55:31 +0200

    rescue routing error

--- app/api/v1/base.rb
+++ app/api/v1/base.rb
@@ -7,5 +7,8 @@ module V1
     error_formatter :json, JsonErrorFormatter
 
     mount Plants
+    route :any, '*path' do
+      error!({ error_code: 404, error_message: "Endpoint #{request.path} Not Found" })
+    end
   end
 end

70ee263ca41f0ab31e41796dfa8885cd1544e275
roland_quaye <rquaye15@hotmail.co.uk>
2023-08-07 17:28:21 +0200

    user session timeout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :timeoutable
   has_many :api_keys
 end
